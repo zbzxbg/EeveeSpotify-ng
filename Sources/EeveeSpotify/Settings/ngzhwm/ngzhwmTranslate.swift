80d76f6d7321b41f1eb8a428667023ab8a95d294
zbzxbg <yuanhainigu@outlook.com>
2026-08-15 08:10:47 +0800

    更新 ngzhwmTranslate.swift

--- Sources/EeveeSpotify/Settings/ngzhwm/ngzhwmTranslate.swift
+++ Sources/EeveeSpotify/Settings/ngzhwm/ngzhwmTranslate.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct NgzhwmTranslateSettingsView: View {
-    // 使用 @AppStorage 自动保存到 UserDefaults，默认值都是 true（开启罗马化）
+
     @AppStorage("ngzhwm_chineseRomanization") var chineseRomanization: Bool = false
     @AppStorage("ngzhwm_japaneseRomanization") var japaneseRomanization: Bool = false
     @AppStorage("ngzhwm_koreanRomanization") var koreanRomanization: Bool = false
